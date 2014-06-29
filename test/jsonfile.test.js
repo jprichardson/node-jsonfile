a62d7921203a4694f84a30ba1a9df43ed97675ab
JP Richardson <jprichardson@gmail.com>
2014-06-29 07:43:32 -0500

    test/jsonfile.test: reformatted to two spaces from four

--- test/jsonfile.test.js
+++ test/jsonfile.test.js
@@ -12,53 +12,53 @@ beforeEach(function() {
 suite('jsonfile')
 
 test('- readFile()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile.json')
-    var obj = {name: 'JP'}
-    fs.writeFileSync(file, JSON.stringify(obj))
+  var file = path.join(TEST_DIR, 'somefile.json')
+  var obj = {name: 'JP'}
+  fs.writeFileSync(file, JSON.stringify(obj))
 
-    jf.readFile(file, function(err, obj2) {
-        F (err)
-        T (obj2.name === obj.name)
-        done()
-    })
+  jf.readFile(file, function(err, obj2) {
+    F (err)
+    T (obj2.name === obj.name)
+    done()
+  })
 })
 
 test('- writeFile()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile2.json')
-    var obj = {name: 'JP'}
+  var file = path.join(TEST_DIR, 'somefile2.json')
+  var obj = {name: 'JP'}
 
-    jf.writeFile(file, obj, function(err) {
-        F (err)
-        fs.readFile(file, 'utf8', function(err,data) {
-            var obj2 = JSON.parse(data)
-            T (obj2.name === obj.name)
-            done()
-        })
+  jf.writeFile(file, obj, function(err) {
+    F (err)
+    fs.readFile(file, 'utf8', function(err,data) {
+      var obj2 = JSON.parse(data)
+      T (obj2.name === obj.name)
+      done()
     })
+  })
 })
 
 test('- readFileSync()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile3.json')
-    var obj = {name: 'JP'}
-    fs.writeFileSync(file, JSON.stringify(obj))
+  var file = path.join(TEST_DIR, 'somefile3.json')
+  var obj = {name: 'JP'}
+  fs.writeFileSync(file, JSON.stringify(obj))
 
-    try {
-        var obj2 = jf.readFileSync(file)
-        T (obj2.name === obj.name)
-        done()
-    } catch (err) {
-        done(err)
-    }
+  try {
+    var obj2 = jf.readFileSync(file)
+    T (obj2.name === obj.name)
+    done()
+  } catch (err) {
+    done(err)
+  }
 })
 
 test('- writeFileSync()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile4.json')
-    var obj = {name: 'JP'}
+  var file = path.join(TEST_DIR, 'somefile4.json')
+  var obj = {name: 'JP'}
 
-    jf.writeFileSync(file, obj)
+  jf.writeFileSync(file, obj)
 
-    var obj2 = JSON.parse(fs.readFileSync(file, 'utf8'))
-    T (obj2.name === obj.name)
-    done()
+  var obj2 = JSON.parse(fs.readFileSync(file, 'utf8'))
+  T (obj2.name === obj.name)
+  done()
 })
 
