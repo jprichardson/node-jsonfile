a922c592fff8ec80315d24ed07867534dc7c0220
JP Richardson <jprichardson@gmail.com>
2014-06-29 07:41:35 -0500

    test/jsonfile.test: removed semicolons

--- test/jsonfile.test.js
+++ test/jsonfile.test.js
@@ -1,64 +1,64 @@
 var testutil = require('testutil')
-  , jf = require('../lib/jsonfile')
-  , fs = require('fs')
-  , path = require('path');
+var jf = require('../')
+var fs = require('fs')
+var path = require('path')
 
 var TEST_DIR = ''
 
 beforeEach(function() {
-  TEST_DIR = testutil.createTestDir('jsonfile');
+  TEST_DIR = testutil.createTestDir('jsonfile')
 })
 
-suite('jsonfile');
+suite('jsonfile')
 
 test('- readFile()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile.json');
+    var file = path.join(TEST_DIR, 'somefile.json')
     var obj = {name: 'JP'}
-    fs.writeFileSync(file, JSON.stringify(obj));
+    fs.writeFileSync(file, JSON.stringify(obj))
 
     jf.readFile(file, function(err, obj2) {
         F (err)
         T (obj2.name === obj.name)
-        done();
-    });
+        done()
+    })
 })
 
 test('- writeFile()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile2.json');
-    var obj = {name: 'JP'};
+    var file = path.join(TEST_DIR, 'somefile2.json')
+    var obj = {name: 'JP'}
 
     jf.writeFile(file, obj, function(err) {
         F (err)
         fs.readFile(file, 'utf8', function(err,data) {
-            var obj2 = JSON.parse(data);
+            var obj2 = JSON.parse(data)
             T (obj2.name === obj.name)
-            done();
-        });
+            done()
+        })
     })
 })
 
 test('- readFileSync()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile3.json');
+    var file = path.join(TEST_DIR, 'somefile3.json')
     var obj = {name: 'JP'}
-    fs.writeFileSync(file, JSON.stringify(obj));
+    fs.writeFileSync(file, JSON.stringify(obj))
 
     try {
-        var obj2 = jf.readFileSync(file);
-        T (obj2.name === obj.name);
-        done();
+        var obj2 = jf.readFileSync(file)
+        T (obj2.name === obj.name)
+        done()
     } catch (err) {
-        done(err);
+        done(err)
     }
 })
 
 test('- writeFileSync()', function(done) {
-    var file = path.join(TEST_DIR, 'somefile4.json');
-    var obj = {name: 'JP'};
+    var file = path.join(TEST_DIR, 'somefile4.json')
+    var obj = {name: 'JP'}
 
-    jf.writeFileSync(file, obj);
+    jf.writeFileSync(file, obj)
 
-    var obj2 = JSON.parse(fs.readFileSync(file, 'utf8'));
-    T (obj2.name === obj.name);
-    done();
+    var obj2 = JSON.parse(fs.readFileSync(file, 'utf8'))
+    T (obj2.name === obj.name)
+    done()
 })
 
