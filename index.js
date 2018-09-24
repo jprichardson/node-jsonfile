617e95153c4da8c36730a32e087ad6b7a9c962eb
TeamTeamwork <dev@teamteam.work>
2018-09-24 03:55:50 +0100

    Alternative, slightly smaller implementation

--- index.js
+++ index.js
@@ -1,137 +1,93 @@
+const universalify = require('universalify')
 let _fs
 try {
   _fs = require('graceful-fs')
 } catch (_) {
   _fs = require('fs')
 }
-const universalify = require('universalify')
 
-function readFileWithCallback (file, options, callback) {
-  if (callback == null) {
-    callback = options
-    options = {}
+function stripBom (content) {
+  if (Buffer.isBuffer(content)) {
+    content = content.toString('utf8')
   }
-
-  if (typeof options === 'string') {
-    options = { encoding: options }
-  }
-
-  options = options || {}
-  const fs = options.fs || _fs
-
-  let shouldThrow = true
-  if ('throws' in options) {
-    shouldThrow = options.throws
-  }
-
-  fs.readFile(file, options, (err, data) => {
-    if (err) return callback(err)
-
-    data = stripBom(data)
-
-    let obj
-    try {
-      obj = JSON.parse(data, options ? options.reviver : null)
-    } catch (err2) {
-      if (shouldThrow) {
-        err2.message = `${file}: ${err2.message}`
-        return callback(err2)
-      } else {
-        return callback(null, null)
-      }
-    }
-
-    callback(null, obj)
-  })
+  return content.replace(/^\uFEFF/, '')
 }
 
-const readFile = universalify.fromCallback(readFileWithCallback)
+function doThrowForFile (file, options, error, callback) {
+  error.message = file + ': ' + error.message
+  return options['throws'] !== false ? callback(error) : callback(null, null)
+}
 
-function readFileSync (file, options) {
-  options = options || {}
-  if (typeof options === 'string') {
-    options = { encoding: options }
-  }
+function throwErrorCallback (err, data) {
+  if (err) throw err
+  return data
+}
 
-  const fs = options.fs || _fs
+function getOptions (options) {
+  return typeof options === 'string' ? { encoding: options } : options || {}
+}
 
-  let shouldThrow = true
-  if ('throws' in options) {
-    shouldThrow = options.throws
-  }
+function handleCallbackAndOptions (options, callback) {
+  return callback != null ? [options, callback] : [{}, options]
+}
 
+function parseJson (file, data, options, callback) {
   try {
-    let content = fs.readFileSync(file, options)
-    content = stripBom(content)
-    return JSON.parse(content, options.reviver)
-  } catch (err) {
-    if (shouldThrow) {
-      err.message = `${file}: ${err.message}`
-      throw err
-    } else {
-      return null
-    }
+    const parsed = JSON.parse(stripBom(data), options['reviver'])
+    return callback(null, parsed)
+  } catch (jsonError) {
+    return doThrowForFile(file, options, jsonError, callback)
   }
 }
 
 function stringify (obj, options) {
-  let spaces
-  let EOL = '\n'
-  if (typeof options === 'object' && options !== null) {
-    if (options.spaces) {
-      spaces = options.spaces
-    }
-    if (options.EOL) {
-      EOL = options.EOL
-    }
-  }
-
-  const str = JSON.stringify(obj, options ? options.replacer : null, spaces)
-
+  const opts = (options && typeof options === 'object') ? options : {}
+  const str = JSON.stringify(obj, opts['replacer'], opts['spaces'])
+  const EOL = opts['EOL'] || '\n'
   return str.replace(/\n/g, EOL) + EOL
 }
 
-function writeFileWithCallback (file, obj, options, callback) {
-  if (callback == null) {
-    callback = options
-    options = {}
-  }
-  options = options || {}
+function readFile (file, options, callback) {
+  [options, callback] = handleCallbackAndOptions(options, callback)
+  options = getOptions(options)
   const fs = options.fs || _fs
+  fs.readFile(file, options, (err, data) => {
+    return err ? callback(err) : parseJson(file, data, options, callback)
+  })
+}
 
-  let str = ''
+function readFileSync (file, options) {
+  options = getOptions(options)
+  const fs = options.fs || _fs
   try {
-    str = stringify(obj, options)
+    const data = fs.readFileSync(file, options)
+    return parseJson(file, data, options, throwErrorCallback)
+  } catch (fileError) {
+    return doThrowForFile(file, options, fileError, throwErrorCallback)
+  }
+}
+
+function writeFile (file, obj, options, callback) {
+  [options, callback] = handleCallbackAndOptions(options || {}, callback)
+  const fs = options.fs || _fs
+  try {
+    const str = stringify(obj, options)
+    fs.writeFile(file, str, options, callback)
   } catch (err) {
     return callback(err, null)
   }
-
-  fs.writeFile(file, str, options, callback)
 }
-
-const writeFile = universalify.fromCallback(writeFileWithCallback)
 
 function writeFileSync (file, obj, options) {
   options = options || {}
   const fs = options.fs || _fs
-
   const str = stringify(obj, options)
-  // not sure if fs.writeFileSync returns anything, but just in case
   return fs.writeFileSync(file, str, options)
 }
 
-function stripBom (content) {
-  // we do this because JSON.parse would convert it to a utf8 string if encoding wasn't specified
-  if (Buffer.isBuffer(content)) content = content.toString('utf8')
-  content = content.replace(/^\uFEFF/, '')
-  return content
+module.exports = {
+  readFile: universalify.fromCallback(readFile),
+  readFileSync: readFileSync,
+  writeFile: universalify.fromCallback(writeFile),
+  writeFileSync: writeFileSync
 }
-
-const jsonfile = {
-  readFile,
-  readFileSync,
-  writeFile,
-  writeFileSync
-}
-
-module.exports = jsonfile
